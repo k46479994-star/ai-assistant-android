87da1b39d538decfed6ed5bd5667464dce8192c6
k46479994-star <k46479994@gmail.com>
2026-08-05 15:10:13 +0900

    test: run layout regression on supported SDK

--- app/src/test/java/com/example/aiassistant/MainActivityLayoutTest.kt
+++ app/src/test/java/com/example/aiassistant/MainActivityLayoutTest.kt
@@ -7,8 +7,10 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [34])
 class MainActivityLayoutTest {
     @Test
     fun aiTabIsAttachedWithFullHeight() {
